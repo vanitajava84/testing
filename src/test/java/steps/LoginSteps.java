9d2820fdf794ce2a63fa9249d09e706df7076d5e
vanitajava84 <vanita.java84@gmail.com>
2025-12-20 16:14:05 +0530

    Update LoginSteps.java

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -13,7 +13,7 @@ public class LoginSteps {
     }
     @When("user enters valid credentials")
     public void userEntersValidCredentials() {
-        System.out.println("user enters valid credentialssss");
+        System.out.println("user enters valid credentialsssss");
     }
     @Then("user should be navigated to the home page")
     public void userShouldBeNavigatedToTheHomePage() {
